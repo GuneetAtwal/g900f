66a77891e3c847a3f22e798a8bb8fe6674f30eae
Andrei F <luxneb@gmail.com>
2013-08-22 16:02:20 +0400

    logger: Add an enable toggle for entry storage
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -37,6 +37,9 @@ static char klog_buf[256];
 #define CONFIG_LOGCAT_SIZE 256
 #endif
 
+static unsigned int enabled = 1;
+module_param(enabled, uint, S_IWUSR | S_IRUGO);
+
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  *
@@ -469,6 +472,9 @@ ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 	ssize_t ret = 0;
+
+	if (!enabled)
+		return 0;
 
 	now = current_kernel_time();
 
