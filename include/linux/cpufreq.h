80bb2c7bd5b22b402200b98f7ca09cd778928ad2
GuneetAtwal <harjinder767@gmail.com>
2014-05-12 20:13:50 +0530

    drivers/cpufreq: Add Lionheart Governor
    Signed-off-by: GuneetAtwal <harjinder767@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -470,7 +470,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplugv2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
->>>>>>> 7e910f1... cpufreq: Add More CPU Governors
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #endif
 
 
