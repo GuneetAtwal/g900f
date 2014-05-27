489eff2f47c8de13b0b76753fcb453b9cb431663
GuneetAtwal <harjinder767@gmail.com>
2014-05-27 22:39:32 +0530

    Revert "hotplug: Fix UP bug in smpboot hotplug code"
    This reverts commit 7689b574646b7d309ee50076b8e383fe7e3b87bc.

--- kernel/smpboot.c
+++ kernel/smpboot.c
@@ -15,8 +15,6 @@
 
 #include "smpboot.h"
 
-#ifdef CONFIG_SMP
-
 #ifdef CONFIG_GENERIC_SMP_IDLE_THREAD
 /*
  * For the hotplug case we keep the task structs around and reuse
@@ -73,8 +71,6 @@ void __init idle_threads_init(void)
 	}
 }
 #endif
-
-#endif /* #ifdef CONFIG_SMP */
 
 static LIST_HEAD(hotplug_threads);
 static DEFINE_MUTEX(smpboot_threads_lock);
